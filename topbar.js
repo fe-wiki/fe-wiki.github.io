d85bf22d204c573a9e2b15673f485acbd0e7e8a4
fe-wiki <krakoix2@gmail.com>
2026-02-23 19:36:11 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,11 +1,11 @@
 var topbarBegin = '<div id="topBar"></div>'
 var topbarList = new Array()
-topbarList.push(["<b>Home</b>","/"])
+topbarList.push(["<b>FE Wiki</b> <font size='8'>Unoffical</font>","/"])
 
 window.addEventListener("load", function() {
   document.body.innerHTML = topbarBegin + document.body.innerHTML
   for (var index = 0; index < topbarList.length; index++) {
     var value = topbarList[index]
-    document.getElementById("topBar").innerHTML += `<a href="${value[1]}">${value[2]}</a>`
+    document.getElementById("topBar").innerHTML += `<a href="${value[1]}">${value[0]}</a>`
   }
 })
