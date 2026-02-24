9390c5a8d8505809e4d036d223c696a54d3e90c3
fe-wiki <krakoix2@gmail.com>
2026-02-23 19:29:58 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -5,6 +5,6 @@ topbarList.push(["<b>Home</b>","/"])
 window.addEventListener("load", function() {
   for (var index = 0; index < topbarList.length; index++) {
     var value = topbarList[index]
-    document.getElementById("topBar").innerHTML += `<a href="${value[1}">${value[2]}</a>`
+    document.getElementById("topBar").innerHTML += `<a href="${value[1]}">${value[2]}</a>`
   }
 })
