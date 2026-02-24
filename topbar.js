ed5c041862db5b352a88f9ce80e2a5de643eab85
fe-wiki <krakoix2@gmail.com>
2026-02-23 19:28:12 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,1 +1,10 @@
-console.log("test")
+var topbarBegin = '<div id="topBar"></div>'
+var topbarList = new Array()
+topbarList.push(["<b>Home</b>","/"])
+
+window.addEventListener("load", function() {
+  for (var index = 0; index < topbarList.length; index++) {
+    var value = topbarList[index]
+    document.getElementById("topBar").innerHTML += `<a href="${value[1}">${value[2]}</a>`
+  }
+})
