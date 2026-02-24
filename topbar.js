3baa065e0cf28eeef3e058f3f9286dd958cac42a
fe-wiki <krakoix2@gmail.com>
2026-02-23 22:16:38 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -11,10 +11,14 @@ window.addEventListener("load", function() {
   for (var index = 0; index < topbarList.length; index++) {
     var value = topbarList[index]
     var extra = ""
+    var emd = ""
+    if (index > 0) {
+      emd = "• "
+    }
     if (window.location.pathname.replace(/\/$/, "") == value[1].replace(/\/$/, "")) {
       extra = "color:#ffffff;text-decoration: underline;"
     }
-    document.getElementById("topBar").innerHTML += `<a style="${extra}" href="${value[1]}">${value[0]}</a> `
+    document.getElementById("topBar").innerHTML += `${emd}<a style="${extra}" href="${value[1]}">${value[0]}</a> `
   }
    document.body.innerHTML += `<br><small>If you'd like to add pages or make changes the wiki, go to <a href="https://github.com/fe-wiki/fe-wiki.github.io">the github repository page</a> and start contributing.</small>`
 })
