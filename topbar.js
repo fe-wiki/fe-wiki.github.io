321716049f744bbd640391fe3f02c20029c38669
fe-wiki <krakoix2@gmail.com>
2026-02-23 19:57:54 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -4,14 +4,12 @@ topbarList.push(["<b>FE Wiki</b>","/"])
 topbarList.push(["About","/about"])
 
 window.addEventListener("load", function() {
-  var fullURL = window.location.href;
-  var fileName = fullURL.split('?')[0].split('#')[0];
   
   document.body.innerHTML = topbarBegin + document.body.innerHTML
   for (var index = 0; index < topbarList.length; index++) {
     var value = topbarList[index]
     var extra = ""
-    if (fileName == value[1]) {
+    if (window.location.pathname == value[1]) {
       extra = "color:#6666ff"
     }
     document.getElementById("topBar").innerHTML += `<a style="${extra}" href="${value[1]}">${value[0]}</a> `
